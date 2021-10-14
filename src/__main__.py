a27d3f1c75c633490e5b44cf5b60329e1dcbd283
R3tt4h <kevin953@outlook.com>
2021-10-14 16:55:24 +0200

    feat: inventory

--- src/__main__.py
+++ src/__main__.py
@@ -8,7 +8,6 @@ from Utils.Perso import Perso
 from Story.initialQuest import InitialQuest
 from Window.MainWindow import MainWindow
 
-
 if __name__ == '__main__':
     print("Bienvenue dans Donjon & Dragon")
     list_perso = Perso.listPerso()
