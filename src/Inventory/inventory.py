fc921285caa1a2cd560b535acc497048cfdfaba6
browngreg <gregory.yazidi@gmail.com>
2022-09-23 15:28:00 +0200

    update inventory frame & manage

--- src/Inventory/inventory.py
+++ src/Inventory/inventory.py
@@ -1,43 +1,35 @@
-from tkinter import Button, Frame, Label
+# from tkinter import Button, Frame, Label
 
 
-class Inventory:
-    __items = None
+# class Inventory:
+#     __items = None
 
-    def __init__(self, perso):
-        self.__items = perso
+#     def __init__(self, perso):
+#         self.__items = perso
 
-    def useItem(self, name, amount, hp):
-        if( name == 'potion'):
-            if (hp + 5 > 20):
-                hp = 20
-                return hp
-            else: return hp + 5
+#     def useItem(self, name, amount, hp):
+#         if( name == 'potion'):
+#             if (hp + 5 > 20):
+#                 hp = 20
+#                 return hp
+#             else: return hp + 5
             
 
-        elif( name == 'super-potion'):
-            if (hp + 10 > 20):
-                hp = 20
-                return hp
-            else: return hp + 10
+#         elif( name == 'super-potion'):
+#             if (hp + 10 > 20):
+#                 hp = 20
+#                 return hp
+#             else: return hp + 10
             
-        elif( name == 'mega-potion'):
-            hp = 20
-            return hp
+#         elif( name == 'mega-potion'):
+#             hp = 20
+#             return hp
 
-    def add_item(self):
-        print("add item")
+#     def add_item(self):
+#         print("add item")
 
-    def remove_item(self):
-        print("remove item")
+#     def remove_item(self):
+#         print("remove item")
 
-    def edit_item(self):
-        print("edit item")
-
-
-
-
-
-
-    
-
+#     def edit_item(self):
+#         print("edit item")
